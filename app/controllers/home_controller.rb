830516de9b611e2e42751a55a55330804f04ad7e
hotuta <hotuta@users.noreply.github.com>
2017-11-26 13:14:04 +0900

    信じる気持ち

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 require 'markDownParse.rb'
+require 'thinreport_helper.rb'
 
 class HomeController < ApplicationController
   def top
@@ -8,7 +9,14 @@ class HomeController < ApplicationController
     file = params[:uppic]
 
     yamlFile =  MarkDownParse.new(file.read)
-    yamlFile = yamlFile.getYamlList()
-    @name = yamlFile['名前']['氏名']
+    @yaml_obj = yamlFile.getYamlList()
+    thinhelper = ThinreportHelper.new(@yaml_obj)
+    # binding.pry
+    thinhelper.gen_pdf("app/assets/jis_rireki0.tlf")
+
+    # thinhelper = ThinreportHelper.init_use_file("../Profile.yml")
+    # thinhelper.gen_pdf("jis_rireki0.tlf")
+    # binding.pry
+    # @name = yamlFile['名前']['氏名']
   end
 end
