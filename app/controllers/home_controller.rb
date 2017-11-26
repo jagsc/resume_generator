a741786ab843ee48456fcbb312ff69254a5beda2
godan <matsushita@godan.xyz>
2017-11-26 11:38:55 +0900

    [tmp] ファイルを取得してYAMLParseするまでを実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
+require 'markDownParse.rb'
+
 class HomeController < ApplicationController
   def top
+  end
+
+  def receiveFile
+    file = params[:uppic]
+
+    yamlFile =  MarkDownParse.new(file.read)
+    yamlFile = yamlFile.getYamlList()
+    @name = yamlFile['名前']['氏名']
   end
 end
