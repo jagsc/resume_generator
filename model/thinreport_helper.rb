d702c9a20beb962b4f283552a1b42ed806ebb269
prprhyt <prprhyt@gmail.com>
2017-11-26 12:20:42 +0900

    [fix] fix variable name json_obj -> yaml_obj

--- model/thinreport_helper.rb
+++ model/thinreport_helper.rb
@@ -6,36 +6,36 @@ require 'yaml'
 
 
 class ThinreportHelper
-  @@json_obj
+  @@yaml_obj
   @@file_name
-  def initialize(json_obj,file_name)
-    @@json_obj = json_obj
+  def initialize(yaml_obj,file_name)
+    @@yaml_obj = yaml_obj
   end
 
   def self.init_use_file(yaml_file_name)
-    json_obj = open(yaml_file_name, 'r') { |f| YAML.load(f) }
+    @@yaml_obj = open(yaml_file_name, 'r') { |f| YAML.load(f) }
     @@file_name = yaml_file_name + ".pdf"
-    new(json_obj,yaml_file_name)
+    new(@@yaml_obj,yaml_file_name)
   end
 
   def gen_pdf(tlf_file_name)
     report = Thinreports::Report.new layout: tlf_file_name
     report.start_new_page
-    report.page.item(:name_rubi).value(@@json_obj.dig("名前","ふりがな"))
-    report.page.item(:name_main).value(@@json_obj.dig("名前","氏名"))
-    #report.page.item(:year).value(@@json_obj.dig("現住所","ふりがな"))
+    report.page.item(:name_rubi).value(@@yaml_obj.dig("名前","ふりがな"))
+    report.page.item(:name_main).value(@@yaml_obj.dig("名前","氏名"))
+    #report.page.item(:year).value(@@yaml_obj.dig("現住所","ふりがな"))
     #report.page.item(:month).value('11')
     #report.page.item(:date).value('26')
     #report.page.item(:old).value('14')
-    report.page.item(:address_rubi).value(@@json_obj.dig("現住所","ふりがな"))
-    report.page.item(:address_main).value(@@json_obj.dig("現住所","住所"))
-    report.page.item(:tel).value(@@json_obj.dig("現住所","TEL"))
-    report.page.item(:tel_mobile).value(@@json_obj.dig("現住所","携帯"))
-    report.page.item(:address_sub_rubi).value(@@json_obj.dig("連絡先の住所","ふりがな"))
-    report.page.item(:address_sub_main).value(@@json_obj.dig("連絡先の住所","住所"))
-    report.page.item(:tel_sub).value(@@json_obj.dig("連絡先の住所","TEL"))
-    report.page.item(:tel_sub_mobile).value(@@json_obj.dig("連絡先の住所","携帯"))
-    user_history = @@json_obj.dig("学歴_職歴")
+    report.page.item(:address_rubi).value(@@yaml_obj.dig("現住所","ふりがな"))
+    report.page.item(:address_main).value(@@yaml_obj.dig("現住所","住所"))
+    report.page.item(:tel).value(@@yaml_obj.dig("現住所","TEL"))
+    report.page.item(:tel_mobile).value(@@yaml_obj.dig("現住所","携帯"))
+    report.page.item(:address_sub_rubi).value(@@yaml_obj.dig("連絡先の住所","ふりがな"))
+    report.page.item(:address_sub_main).value(@@yaml_obj.dig("連絡先の住所","住所"))
+    report.page.item(:tel_sub).value(@@yaml_obj.dig("連絡先の住所","TEL"))
+    report.page.item(:tel_sub_mobile).value(@@yaml_obj.dig("連絡先の住所","携帯"))
+    user_history = @@yaml_obj.dig("学歴_職歴")
     counter = 0
     user_history.each{|element|
       detail,year_month = element.shift
@@ -48,9 +48,10 @@ class ThinreportHelper
       ++counter
     }
     report.generate(filename: @@file_name)
+    puts "Done"
   end
 end
 
 #test
-thinhelper = ThinreportHelper.init_use_file("Profile.yml")
+thinhelper = ThinreportHelper.init_use_file("../Profile.yml")
 thinhelper.gen_pdf("jis_rireki0.tlf")
